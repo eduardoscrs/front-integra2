5255cf966e26fb4fecc32632935d8718aca48f39
Eduardo Escares <eduardoscrs60@gmail.com>
2024-09-04 22:27:53 -0400

    navbar simple, paginas

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Cerrar Sesion</Link>
+        </li>
+        <li>
+          <Link to="/ingreso-formulario">Ingreso Formulario</Link>
+        </li>
+        <li>
+          <Link to="/casos">Casos</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
