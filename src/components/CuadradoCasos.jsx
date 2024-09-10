20d1def37368a4f991002d730952c06f482d2275
ConstanzaOyarzo <coyarzod0@gmail.com>
2024-09-10 08:27:13 -0300

    Componente tarjeta cuadrado casos

---
+++ src/components/CuadradoCasos.jsx
@@ -0,0 +1,11 @@
+function CuadradoCasos() {
+    return (
+        <div className="cuadrado_casos"> {/* cuadrado contenedor */}
+            <h2>{titulo}</h2> {/* titulo */}
+            <img src="" alt="Icono" /> {/* icono */}
+            <p>{numero_casos}</p> {/* numero en grande */}
+        </div>
+    );
+}
+
+export default CuadradoCasos;
--- src/pages/Casos.jsx
+++ src/pages/Casos.jsx
@@ -1,9 +1,14 @@
+import CuadradoCasos from "../components/CuadradoCasos";
+
 const Casos = () => {
   return (
     <div>
       <h1>Casos Pendientes</h1>
+      <CuadradoCasos></CuadradoCasos>
     </div>
   );
 };
 
 export default Casos;
+
+/* exportar componente cuadrado_sup_casos  */
