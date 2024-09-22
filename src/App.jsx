3b16ea77f53b9239a726cba2d5f5380defe6b541
saezjose <134319766+saezjose@users.noreply.github.com>
2024-09-21 23:18:27 -0300

    Problema con el componente Sidebar y login arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-{/*import Sidebar from './components/Sidebar';*/}
+import Sidebar from './components/Sidebar';
 import Login from './components/Login';
 import './styles/login.css';
-
 
 function App() {
   return (
     <Router>
       <div className="App">
-       {/*<Sidebar />*/}
-        <div className="main-content">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            {/* Otras rutas pueden ir aquí */}
-          </Routes>
-        </div>
+        <Routes>
+          {/* Ruta para la página de inicio o raíz */}
+          <Route 
+            path="/" 
+            element={
+              <>
+                <Sidebar />
+                <div className="main-content">
+                  {/* Aquí puedes poner contenido adicional o componentes para la página principal */}
+                </div>
+              </>
+            } 
+          />
+          
+          {/* Ruta para la página de login */}
+          <Route 
+            path="/login" 
+            element={<Login />} 
+          />
+          
+          {/* Puedes agregar más rutas aquí */}
+          
+        </Routes>
       </div>
     </Router>
   );
