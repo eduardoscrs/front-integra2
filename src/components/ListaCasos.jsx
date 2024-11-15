f3b5f62d82cf28cfb814bd5ca7a9840e2e09576e
ConstanzaOyarzo <coyarzod0@gmail.com>
2024-11-15 17:16:29 -0300

    Ajuste estilo pdf

--- src/components/ListaCasos.jsx
+++ src/components/ListaCasos.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 function ListaCasos({
   numeroCaso,
@@ -11,21 +12,56 @@ function ListaCasos({
   const descargarPDF = () => {
     const doc = new jsPDF();
 
-    // Agregar contenido al PDF
-    doc.text(`Caso ID: ${numeroCaso}`, 10, 10);
-    doc.text(`Tipo de siniestro: ${datosCaso.tipo_siniestro}`, 10, 20);
-    doc.text(`Descripción: ${datosCaso.descripcion}`, 10, 30);
-    doc.text(`Estado: ${estadoCaso}`, 10, 40);
+    // Fuentes
+    // doc.addFont('helvetica', 'helvetica', 'normal');
+    // Cuidado al añadir fuentes
+    doc.setFont('helvetica');
+
+    // Título
+    doc.setFontSize(22);
+    doc.setTextColor(0, 102, 204);
+    doc.text('Informe del Caso', 105, 30, { align: 'center' });
+
+    // Información del caso
+    doc.setFontSize(14);
+    doc.setTextColor(0, 0, 0);
+    doc.text(`ID del caso: ${numeroCaso}`, 20, 50);
+
+    // Tabla de datos
+    const tableData = [
+      ['Tipo de siniestro', datosCaso.tipo_siniestro],
+      ['Descripción', datosCaso.descripcion],
+      ['Estado', estadoCaso],
+    ];
+
+    doc.autoTable({
+      startY: 60,
+      head: [['Categoria', 'Detalle']],
+      body: tableData,
+      theme: 'striped',
+      headStyles: { fillColor: [0, 102, 204] },
+    });
+
+    // Línea separadora
+    // const finalY = doc.previousAutoTable.finalY || 150;
+    // doc.setDrawColor(0, 102, 204);
+    // doc.line(20, finalY + 10, 190, finalY + 10);
+
+    // Pie de página
+    doc.setFontSize(10);
+    doc.setTextColor(128);
+    doc.text(`Generado el: ${new Date().toLocaleString()}`, 20, 280);
+    doc.text('Página 1 de 1', 190, 280, { align: 'right' });
 
     // Descargar el PDF
-    doc.save(`Caso_${numeroCaso}.pdf`);
+    doc.save(`Informe_Caso${numeroCaso}.pdf`);
   };
 
   return (
     <div className="lista-caso">
-      <div onClick={descargarPDF}>
+      <div>
         {/* <span className="circulo-caso"></span> */}
-        <a className="link-caso" download={'Caso'}>
+        <a className="link-caso" onClick={descargarPDF}>
           <p>Caso {numeroCaso}</p>
         </a>
       </div>
@@ -47,7 +83,7 @@ ListaCasos.propTypes = {
   estadoCaso: PropTypes.string.isRequired,
   onAceptar: PropTypes.func.isRequired,
   onRechazar: PropTypes.func.isRequired,
-  datosCaso: PropTypes.string.isRequired,
+  datosCaso: PropTypes.object.isRequired,
 };
 
 export default ListaCasos;
