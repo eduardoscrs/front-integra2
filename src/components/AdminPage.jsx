f3d39f10c4da3f31bcfc99c342c547555ea37b6e
saezjose <134319766+saezjose@users.noreply.github.com>
2024-11-18 14:11:42 -0300

    cambios en el diseño de la pagina del administrador

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -33,7 +33,44 @@ const AdminPage = () => {
           </div>
         </div>
 
-        <button className="quick-action__button">Añadir nuevo Caso</button>
+        <div className="main-info">
+          <div className="box">
+          </div>
+        </div>
+
+        <div className="main-info">
+          <div className="box2">
+          </div>
+        </div>
+
+        <div className="main-info">
+          <div className="box3">
+          <iframe
+            width="350"
+            height="300"
+            src="https://www.youtube.com/embed/6y-BJPlTGic"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          </div>
+        </div>
+
+        <div className="main-info">
+          <div className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5140.765314279909!2d-72.55041642268066!3d-38.702891171766254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9614d25bb3523b6b%3A0x6d64cbb854e11a5b!2sUniversidad%20Cat%C3%B3lica%20de%20Temuco%20-%20Campus%20San%20Juan%20Pablo%20Segundo%2C%20IX%20Regi%C3%B3n%2C%20Chile!5e1!3m2!1ses-419!2scl!4v1731949017879!5m2!1ses-419!2scl"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+          </div>
+        </div>
+
+         
 
         <div className="date-time">
           <h2>{dayOfWeek}</h2>
