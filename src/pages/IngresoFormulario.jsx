0e754c3e9c6694d564edd161a85797a7613faf07
saezjose <134319766+saezjose@users.noreply.github.com>
2024-11-20 17:29:13 -0300

    implementacion del sidebar en ingreso formulario.

--- src/pages/IngresoFormulario.jsx
+++ src/pages/IngresoFormulario.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import '../styles/Formulario.css';
 import { crearCaso } from '../services/formularioService';
+import { Outlet, Link } from "react-router-dom"; // Importa Link para redirigir
+import '../styles/Sidebar.css';
+import logo from '../assets/Segurapp_rbg.png'; 
 
 const IngresoFormulario = () => {
   const [formData, setFormData] = useState({
@@ -68,8 +71,71 @@ const IngresoFormulario = () => {
     enviarDatos();
   };
 
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true); // Sidebar se abre
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false); // Sidebar se cierra
+  };
+
   return (
     <div className="form-container">
+
+<div
+      className={`sidebar ${isOpen ? "sidebar--open" : "sidebar--closed"}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {/* Logo */}
+      <div className="sidebar__logo">
+        <img src={logo} alt="Logo" />
+      </div>
+
+      {/* Menu */}
+      <ul className="sidebar__menu">
+        <li>
+          <Link to="/inicio">
+            <i className="icon-dashboard"></i>Inicio
+          </Link>
+        </li>
+        <li>
+          <Link to="/ingreso-formulario">
+            <i className="icon-dashboard"></i>Formulario
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/casos">
+            <i className="icon-products"></i> Casos
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="/perfil-usuario">
+            <i className="icon-analytics"></i> Perfil
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/logout">
+            <i className="icon-analytics"></i> Logout
+          </Link>
+        </li>
+
+      </ul>
+
+      {/* Login Section */}
+      <div className="sidebar__login">
+        <Link to="/login">
+          <button className="login__button">Login</button>
+        </Link>
+      </div>
+      <Outlet/>
+    </div>
+    
       <form id="case-form" onSubmit={handleSubmit} noValidate>
         <h2>Formulario de Caso</h2>
 
